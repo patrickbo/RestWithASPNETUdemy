7618f72631fac8e4182401f7d25923151c621874
Patrick <patrick@ziad.com.br>
2019-01-05 22:30:16 -0200

    Starting Implementing

--- 02UsingDiferentVerbs/02UsingDiferentVerbs/Controllers/PersonController.cs
+++ 02UsingDiferentVerbs/02UsingDiferentVerbs/Controllers/PersonController.cs
@@ -8,7 +8,7 @@ namespace _02UsingDiferentVerbs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonController : ControllerBase
     {
         // GET api/values
         [HttpGet]
